c054e85d684c7f1fdc3d0af4252a6bd06445705b
Earl Platt III <36335481+eaplatt3@users.noreply.github.com>
2019-04-04 20:44:17 -0400

    inital commit

---
+++ Assets/Rotate.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotate : MonoBehaviour
+{
+    public float spd = 1.0f;
+    public float mspd = .1f;
+    private bool flge = false;
+
+    public Camera camera1;
+    public Camera camera2;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+              
+        
+        transform.Rotate(new Vector3(0, Input.GetAxisRaw("vc")*spd, 0));
+        transform.Translate(new Vector3(Input.GetAxisRaw("cc"), 0, 0) * mspd);
+       
+
+        if (Input.GetKeyDown("1")) { flge = !flge; }
+        
+            camera1.enabled = !flge;
+            camera2.enabled = flge;
+       
+
+        
+
+    }
+}
---
+++ Assets/Shooting.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shooting : MonoBehaviour
+{
+
+    public Rigidbody projectile;
+    public Camera camera1;
+    public Camera camera2;
+    public int speed = 50;
+    public float angle = 10.0f;
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (Input.GetKey("1"))
+        {
+            camera1.enabled = false;
+            camera2.enabled = true;
+
+        }
+        else if (Input.GetKey("2"))
+        {
+            camera2.enabled = false;
+            camera1.enabled = true;
+
+        }
+
+        if (Input.GetKey("3"))
+        {
+            angle -= 0.1f;
+        }
+        Vector3 v = new Vector3(Input.GetAxisRaw("Horizontal"), -1f,
+                            Input.GetAxisRaw("Vertical"));
+
+
+        GetComponent<Rigidbody>().AddForce(v * 20);
+
+        transform.Rotate(new Vector3(0, Input.GetAxisRaw("vc") * 10, 0));
+
+
+        if (Input.GetButtonDown("Fire1"))
+        {
+            // Instantiate the projectile at the position and rotation of this transform
+            Rigidbody clone;
+            clone = Instantiate(projectile, transform.position, transform.rotation);
+
+            // Give the cloned object an initial velocity along the current
+            // object's Z axis
+            clone.velocity = transform.TransformDirection(Vector3.forward * speed + Vector3.up * angle);
+            Object.Destroy(clone, 2.0f);
+        }
+    }
+}
